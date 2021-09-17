2aff68d946e38c535236900f72fdbc1d835dff9e
Olexii Drabchak <73908017+whoiam-bat@users.noreply.github.com>
2021-09-17 11:23:58 +0300

    while true loop

--- lab11/lab11_B/lab11_B.cpp
+++ lab11/lab11_B/lab11_B.cpp
@@ -5,24 +5,28 @@ int main()
     int a, h;
     cout << "1 to find square of circle\n";
     cout << "2 to find square of triangle\n";
-    int choice;
-    cin >> choice;
-    switch (choice)
-    {
-    case 1: {
-        cout << "\nEnter radius: ";
-        cin >> a;
-        Figure object1(a);
-        break;
-    }
-    case 2: {
-        cout << "Enter side: ";
-        cin >> a;
-        cout << "\nEnter height: ";
-        cin >> h;
-        Figure object2(a, h);
-        break;
-    }
+    while (true) {
+        int choice;
+        cin >> choice;
+        switch (choice)
+        {
+        case 1: {
+            cout << "\nEnter radius: ";
+            cin >> a;
+            Figure object1(a);
+            cout << "\n";
+            break;
+        }
+        case 2: {
+            cout << "Enter side: ";
+            cin >> a;
+            cout << "\nEnter height: ";
+            cin >> h;
+            Figure object2(a, h);
+            cout << "\n";
+            break;
+        }
+        }
     }
     return 0;
 }
