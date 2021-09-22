100d50dc020898b854339549fc3de8f7575ad068
Olexii Drabchak <73908017+whoiam-bat@users.noreply.github.com>
2021-09-22 10:47:28 +0300

    update G lab

--- lab11/lab11G/lab11G.cpp
+++ lab11/lab11G/lab11G.cpp
@@ -42,19 +42,13 @@ public:
     string getNameMaster();
     int getID();
 
-    void setWeightDog(int indexDog, int weight) {
-        dogs[indexDog - 1].setWeight(weight);
-    }
-    void setAgeDog(int indexDog, int age) {
-        dogs[indexDog - 1].setAge(age);
-    }
-    void setNameDog(int indexDog, string name) {
-        dogs[indexDog - 1].setName(name);
-    }
-
-    void printMastersList(int amountPeoples);
-    void printDogs(int countDogs);
+    void setWeightDog(int indexDog, int weight);
+    void setAgeDog(int indexDog, int age);
+    void setNameDog(int indexDog, string name);
+    void printMastersList();
+    void printDogs();
 };
+
 int Master::count = 0;
 void menu();
 
@@ -77,7 +71,7 @@ int main()
         case 1: {
             for (int i = 0; i < amountPeoples; i++) {
                 cout << "\n\n" << i + 1 << " master:\n";
-                master[i].printMastersList(amountPeoples);
+                master[i].printMastersList();
             }
             break;
         }
@@ -154,16 +148,6 @@ int main()
 }
 
 
-void menu() {
-    cout << "1. to print List;\n";
-    cout << "2. to change master name;\n";
-    cout << "3. to change dog name;\n";
-    cout << "4. to change weight dog;\n";
-    cout << "5. to change age dog;\n";
-    cout << "6. exit from loop;\n";
-    cout << "7. to clear console;\n";
-}
-
 Dog::Dog() {
     name = names[rand() % 7];
     weight = rand() % (15 - 1 + 1) + 1;
@@ -202,7 +186,7 @@ Master::Master() {
 }
 void Master::inputCountDogs() {
     cout << "Enter count of dogs " << count << " master: ";
-    cin >> this->amountDogs;
+    cin >> amountDogs;
 }
 int Master::getCountDogs()
 {
@@ -219,21 +203,41 @@ int Master::getID()
 {
     return id;
 }
-void Master::printMastersList(int amountPeoples) {
+void Master::setWeightDog(int indexDog, int weight)
+{
+    dogs[indexDog - 1].setWeight(weight);
+}
+void Master::setAgeDog(int indexDog, int age)
+{
+    dogs[indexDog - 1].setAge(age);
+}
+void Master::setNameDog(int indexDog, string name)
+{
+    dogs[indexDog - 1].setName(name);
+}
+void Master::printMastersList() {
     cout << "Name: " << getNameMaster() << "  ";
     cout << "ID: " << getID() << "  ";
     cout << "Dogs: " << getCountDogs() << "\n";
-    printDogs(getCountDogs());
+    printDogs();
 
 }
-void Master::printDogs(int countDogs)
+void Master::printDogs()
 {
     cout << "\n";
-    for (int i = 0; i < countDogs; i++) {
+    for (int i = 0; i < getCountDogs(); i++) {
         cout << dogs[i].getName() << "  ";
         cout << "Age: " << dogs[i].getAge() << "  ";
         cout << "Weight: " << dogs[i].getWeight() << "\n";
     }
 }
 
-
+void menu() {
+    cout << "1. to print List;\n";
+    cout << "2. to change master name;\n";
+    cout << "3. to change dog name;\n";
+    cout << "4. to change weight dog;\n";
+    cout << "5. to change age dog;\n";
+    cout << "6. exit from loop;\n";
+    cout << "7. to clear console;\n";
+}
