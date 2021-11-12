49916170b0b98b3a5a9e9613f9c75d0dd920ba04
Olexii Drabchak <73908017+whoiam-bat@users.noreply.github.com>
2021-11-12 20:23:52 +0200

    lab 8B

--- lab8/lab8B/lab8B/Number.h
+++ lab8/lab8B/lab8B/Number.h
@@ -7,6 +7,7 @@ class Number {
 	T var;
 public:
 	Number(T var);
+	~Number();
 	Number<T> operator + (Number<T> obj2);
 	Number<T> operator - (Number<T> obj2);
 	Number<T> operator * (Number<T> obj2);
